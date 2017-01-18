c734c156fc5e651a90e8001375a7ace5d73a3cc5
nitramnoe <eon.martin@icloud.com>
2017-01-18 21:58:41 +0100

    'improve slideshow'

--- main.js
+++ main.js
@@ -2,12 +2,11 @@
   var headerBg = document.querySelector('#header');
   var img = ['photos/photoretouch/cover1.jpg', 'photos/photoretouch/cover2.jpg','photos/photoretouch/cover3.jpg', 'photos/photoretouch/cover4.jpg', 'photos/photoretouch/cover5.jpg', 'photos/photoretouch/cover6.jpg'];
   var currentBg = 0;
-  var bgImg = new Image();
-  bgImg = img[currentBg]
-  bgImg.onload = sliderShow;
-  var sliderShow = setInterval(function nextSlide(){
-    headerBg.style.backgroundImage = 'url(' + bgImg + ')';
+  console.log('first', currentBg);
+  img[currentBg].onload = setInterval(function nextSlide(){
+    headerBg.style.backgroundImage = 'url(' + img[currentBg] + ')';
+    currentBg = currentBg + 1
+    console.log('second', currentBg);
     if (currentBg == img.length) {currentBg = 0;};
-    bgImg.src = img[currentBg + 1];
   }, 5000);
 })()

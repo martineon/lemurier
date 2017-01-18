4da722d460136b9464526ea7d436862f8c349a3a
nitramnoe <eon.martin@icloud.com>
2017-01-18 16:57:22 +0100

    'fix js'

--- main.js
+++ main.js
@@ -3,11 +3,12 @@
   var img = ['photos/photoretouch/cover1.jpg', 'photos/photoretouch/cover2.jpg','photos/photoretouch/cover3.jpg', 'photos/photoretouch/cover4.jpg', 'photos/photoretouch/cover5.jpg', 'photos/photoretouch/cover6.jpg'];
   var currentBg = 0;
   var bgImg = new Image();
-  bgImg.src = img[currentBg +1];
+  bgImg = img[currentBg]
   bgImg.onload = sliderShow;
   var sliderShow = setInterval(function nextSlide(){
-    headerBg.style.backgroundImage = 'url(' + img[currentBg] + ')';
+    headerBg.style.backgroundImage = 'url(' + bgImg + ')';
     currentBg = currentBg + 1;
     if (currentBg == img.length) {currentBg = 0;};
+    bgImg.src = img[currentBg + 1];
   }, 5000);
 })()

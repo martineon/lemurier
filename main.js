6e51d2bbe0a2244db4c492e51c060c1a03fe5a67
nitramnoe <eon.martin@icloud.com>
2017-01-18 22:00:07 +0100

    'slideshow fix'

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
   var img = ['photos/photoretouch/cover1.jpg', 'photos/photoretouch/cover2.jpg','photos/photoretouch/cover3.jpg', 'photos/photoretouch/cover4.jpg', 'photos/photoretouch/cover5.jpg', 'photos/photoretouch/cover6.jpg'];
   var currentBg = 0;
   console.log('first', currentBg);
-  img[currentBg].onload = setInterval(function nextSlide(){
+  img.onload = setInterval(function nextSlide(){
     headerBg.style.backgroundImage = 'url(' + img[currentBg] + ')';
     currentBg = currentBg + 1
     console.log('second', currentBg);

03fe37b0003ade482b29aa97bb8125fbc7c7c930
nitramnoe <eon.martin@icloud.com>
2017-01-18 15:54:23 +0100

    'try to improve slider'

--- main.js
+++ main.js
@@ -2,11 +2,12 @@
   var headerBg = document.querySelector('#header');
   var img = ['photos/photoretouch/cover1.jpg', 'photos/photoretouch/cover2.jpg','photos/photoretouch/cover3.jpg', 'photos/photoretouch/cover4.jpg', 'photos/photoretouch/cover5.jpg', 'photos/photoretouch/cover6.jpg'];
   var currentBg = 0;
+  var bgImg = new Image()
   var slideInterval = setInterval(nextSlide,5000);
   function nextSlide(){
-    headerBg.style.backgroundImage = 'url(' + img[currentBg] + ')';
+    bgImg = img[currentBg];
+    headerBg.style.backgroundImage = 'url(' + bgImg + ')';
     currentBg = currentBg + 1;
-
     if (currentBg == img.length) {
       currentBg = 0;
     };

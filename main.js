45786cd7a0f1e43907dec9e157240a289d9b8aaf
nitramnoe <eon.martin@icloud.com>
2017-01-18 16:47:47 +0100

    'fix js'

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
   var headerBg = document.querySelector('#header');
   var img = ['photos/photoretouch/cover1.jpg', 'photos/photoretouch/cover2.jpg','photos/photoretouch/cover3.jpg', 'photos/photoretouch/cover4.jpg', 'photos/photoretouch/cover5.jpg', 'photos/photoretouch/cover6.jpg'];
   var currentBg = 0;
-  var bgImg = new Image()
-  bgImg.src = img[currentBg +1]
+  var bgImg = new Image();
+  bgImg.src = img[currentBg +1];
   bgImg.onload = sliderShow;
   var sliderShow = setInterval(function nextSlide(){
     headerBg.style.backgroundImage = 'url(' + img[currentBg] + ')';

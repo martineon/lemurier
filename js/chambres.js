c80e3f315132c9a704a1ac4ad92fd4fe7917d7b2
nitramnoe <eon.martin@icloud.com>
2017-01-28 16:40:16 +0100

    change chambre ui

--- js/chambres.js
+++ js/chambres.js
@@ -1,4 +1,17 @@
 function showMore(param) {
   var photoBox = param.previousElementSibling;
   photoBox.classList.toggle('shown')
+};
+
+function zoom(param){
+  var squarePhoto = param.parentElement;
+  var zoomEl = document.querySelector('.zoom')
+  zoomContainer = document.createElement('div');
+  if (zoomEl) {
+    zoomEl.parentNode.removeChild(zoomEl);
+  }else{
+    zoomContainer.innerHTML = '<div class="zoom square-photo" style="cursor:zoom-out" onclick="zoom(this)"><img class="img-zoom" src=\"'+ param.src +'\"></div>' ;
+    var header = document.querySelector('.header');
+    document.body.insertBefore(zoomContainer, header);
+  }
 }
